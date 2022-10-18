150ddc72e6ff3f12e6ffb25e82078ff62a9c02ed
Calen Jackman <calenjackman1@gmail.com>
2022-10-18 19:07:04 -0400

    completed project

--- opt/character_codes.py
+++ opt/character_codes.py
@@ -1,15 +1,18 @@
 import curses
 import locale
 
+
 def main(stdscr):
 
     while True:
         input_character = stdscr.getch()
+
+        if input_character == 27:
+            break
+
         stdscr.clear()
         stdscr.addstr(str(input_character))
         stdscr.refresh()
 
-locale.setlocale()
-code = locale.getpreferredencoding()
 
 curses.wrapper(main)
--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@
 
 import curses
 import time
+import random
+
 
 def start_screen(stdscr):
     stdscr.clear()
@@ -10,42 +12,75 @@ def start_screen(stdscr):
     stdscr.refresh()
     stdscr.getkey()
 
+
+def display_text(stdscr, target, current, wpm=0):
+    stdscr.addstr(target)
+    stdscr.addstr(1, 0, "WPM: {wpm_display}".format(wpm_display=wpm))
+
+    for i, char in enumerate(current):
+        character_correct = target[i] == current[i]
+        color = curses.color_pair(1) if character_correct \
+            else curses.color_pair(2)
+        stdscr.addstr(0, i, char, color)
+
+
+def load_text(file_path):
+    with open(file_path, "r") as f:
+        lines = f.readlines()
+        return random.choice(lines).strip()
+
+
 def wpm_test(stdscr):
-    target_text = 'Hello world this is some test text for this application!'
+    target_text = load_text("../samples.txt")
     current_text = []
+    wpm = 0
+    start_time = time.time()
+
+    stdscr.nodelay(True)
 
     while True:
+        time_elapsed = max(time.time() - start_time, 1)
+        # assuming that the average length of a word is 5 characters
+        wpm = round(((len(current_text) / time_elapsed)*60)/5)
+
         stdscr.clear()
-        stdscr.addstr(target_text)
-
-        for char in current_text:
-            stdscr.addstr(char, curses.color_pair(1))
-
+        display_text(stdscr, target_text, current_text, wpm)
         stdscr.refresh()
 
-        input_char_code = stdscr.getch()
-
-        if input_char_code == 27:
+        if "".join(current_text) == target_text:
+            stdscr.nodelay(False)
             break
 
-        input_key = chr(input_char_code)
+        try:
+            input_key = stdscr.getkey()
+        except:
+            continue
 
-        if input_key in ('KEY_BACKSPACE', '\b', '\x7f'):
+        if ord(input_key) == 27:
+            break
+
+        if input_key in ("KEY_BACKSPACE", '\b', '\x7f'):
             if len(current_text) > 0:
                 current_text.pop()
-        else:
+        elif len(current_text) < len(target_text):
             current_text.append(input_key)
-
 
 
 def main(stdscr):
     stdscr = stdscr
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     start_screen(stdscr)
-    wpm_test(stdscr)
+    while True:
+        wpm_test(stdscr)
+        stdscr.addstr(
+            2, 0, "You completed the text! Press any key to continue...")
+        key = stdscr.getkey()
+        if ord(key) == 27:
+            break
+
 
 if __name__ == '__main__':
     curses.wrapper(main)
