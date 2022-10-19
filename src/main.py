af21e8ffab0c1b9ba064f0e8d217f549177d3ef6
excalebur <calenjackman1@gmail.com>
2022-10-19 18:44:47 -0400

    chdir to working dir

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@
 import curses
 import time
 import random
+import os
 
 
 def start_screen(stdscr):
@@ -67,6 +68,7 @@ def wpm_test(stdscr):
 
 
 def main(stdscr):
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
     stdscr = stdscr
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
